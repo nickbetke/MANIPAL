5ec4c8ed22ad1527ed8af6e6ecb595f2d8300763
Nikhil Btke <31682853+nickbetke@users.noreply.github.com>
2020-09-05 21:10:11 +0530

    Add files via upload

---
+++ one6assignment/src/main/java/zero/one/Address.java
@@ -0,0 +1,31 @@
+package zero.one;
+
+public class Address {
+	private int streetNo;
+	private String city;
+	private String pin;
+	
+	public int getStreetNo() {
+		return streetNo;
+	}
+	public void setStreetNo(int streetNo) {
+		this.streetNo = streetNo;
+	}
+	public String getCity() {
+		return city;
+	}
+	public void setCity(String city) {
+		this.city = city;
+	}
+	public String getPin() {
+		return pin;
+	}
+	public void setPin(String pin) {
+		this.pin = pin;
+	}
+	@Override
+	public String toString() {
+		return "Address [streetNo=" + streetNo + ", city=" + city + ", pin=" + pin + "]";
+	}
+	
+}
